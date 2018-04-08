fc1b5962694063b902b7b6761958c4c2a56f332d
Leocone <wangliang@testbird.com>
2018-04-08 17:55:18 +0800

    colorPercentage.py

--- colorPercentage.py
+++ colorPercentage.py
@@ -3,13 +3,31 @@
 import sys
 import datetime
 
-RED   = "\033[1;31m"  
+RED   = "\033[1;31m"
 BLUE  = "\033[1;34m"
 CYAN  = "\033[1;36m"
 GREEN = "\033[0;32m"
+YELLOW = "\033[0;33m"
 RESET = "\033[0;0m"
 BOLD    = "\033[;1m"
 REVERSE = "\033[;7m"
+def chineseWeek(index) :
+    if index == 0:
+        return '星期一'
+    if index == 1:
+        return '星期二'
+    if index == 2:
+        return '星期三'
+    if index == 3:
+        return '星期四'
+    if index == 4:
+        return '星期五'
+    if index == 5:
+        return '星期六'
+    if index == 6:
+        return '星期天'
+
+
 def printProgressBar (index, iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█'):
     """
     Call in a loop to create terminal progress bar
@@ -22,33 +40,45 @@ def printProgressBar (index, iteration, total, prefix = '', suffix = '', decimal
         length      - Optional  : character length of bar (Int)
         fill        - Optional  : bar fill character (Str)
     """
-    if iteration < index:
+    if index <= 0 or iteration <= 0:
+        return
+    elif iteration < index - 6:
+        return
+    elif iteration < index:
         sys.stdout.write(RED)
     elif iteration == index:
+        #sys.stdout.write(YELLOW)
         sys.stdout.write(GREEN)
-    #else:
-    #    sys.stdout.write(REVERSE)
+    elif iteration < index + 7:
+        print('')
+        return
+    #    sys.stdout.write(GREEN)
+    else:
+        return
+
     percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
     filledLength = int(length * iteration // total)
     bar = fill * filledLength + '-' * (length - filledLength)
-    print('\r%3s / %s %s |%s| %s%% %s' % (iteration, total, prefix, bar, percent, suffix))
+    print(' \r%3s / %s %s |%s| %s%% %s' % (iteration, total, prefix, bar, percent, suffix))
     # Print New Line on Complete
-    if iteration == total: 
+    if iteration == total:
         print()
 
 def indexOfTodayInYear() :
     now = datetime.datetime.now()
-    print now.year, now.month, now.day, now.hour, now.minute, now.second
-    p = [31,28,31,30,31,30,31,31,30,31,30,31] # 平年
-    w = [31,29,31,30,31,30,31,31,30,31,30,31] # 闰年
-    
+    #print now.year, now.month, now.day, now.hour, now.minute, now.second
+    #p = [31,28,31,30,31,30,31,31,30,31,30,31] # 平年
+    #w = [31,29,31,30,31,30,31,31,30,31,30,31] # 闰年
+
     #year =int(input("请输入年："+'\n'))
     #month =int(input("请输入月："+'\n'))
     #day=int(input("请输入日："+'\n'))
     year = now.year
     month = now.month
     day = now.day
-    
+
+    strX = chineseWeek(now.weekday())
+
     arr=[31,28,31,30,31,30,31,31,30,31,30,31]
     total=365
     sum=day
@@ -56,37 +86,42 @@ def indexOfTodayInYear() :
         sum+=arr[i]
     if year%4==0:
         if year%100==0 and year%400!=0:
-            print("这是今年的第%d天" % sum)
+            print ("\n\n\n\n\n                    %s年%s月%s日 %s 这是今年的第%d天" % (now.year, now.month, now.day, strX,  sum))
+            #print("这是今年的第%d天" % sum)
         else:
             sum=sum+1
             total=total+1
-            print("这是今年的第%d天" % sum)
+            print ("\n\n\n\n\n                    %s年%s月%s日 %s 这是今年的第%d天" % (now.year, now.month, now.day, strX,  sum))
+            #print("这是今年的第%d天" % sum)
     else:
-        print("这是今年的第%d天" % sum)
-    
+        print ("\n\n\n\n\n                  %s年%s月%s日 %s 这是今年的第%d天" % (now.year, now.month, now.day, strX,  sum))
+        #print("这是今年的第%d天" % sum)
+
     ret = []
     ret.append(total)
     ret.append(sum)
     return ret
-# 
+#
 # Sample Usage
-# 
+#
 
-from time import sleep
+#from time import sleep
 
+print ""
 yearDays,indexOfYear = indexOfTodayInYear()
-print yearDays
-print indexOfYear
+#print yearDays
+#print indexOfYear
 # A List of Items
 items = list(range(0, int(yearDays)))
 l = len(items)
 
 # Initial call to print 0% progress
-printProgressBar(indexOfTodayInYear,0, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
+printProgressBar(indexOfTodayInYear,0, l, prefix = ':', suffix = '', length = 50)
 for i, item in enumerate(items):
     # Do stuff...
-    sleep(0.1)
+    #sleep(0.1)
     # Update Progress Bar
-    printProgressBar(indexOfYear, i + 1, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
+    printProgressBar(indexOfYear, i + 1, l, prefix = ':', suffix = '', length = 50)
 
 
+print ""
