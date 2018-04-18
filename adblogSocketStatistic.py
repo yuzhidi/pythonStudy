000dc3d0a400db87a465fbec177b4cfc641ba08d
Leocone <wangliang@testbird.com>
2018-04-18 08:08:34 +0800

    grep -E LS

--- adblogSocketStatistic.py
+++ adblogSocketStatistic.py
@@ -10,7 +10,9 @@ import re
 patternRS = re.compile("- RS\((\\d+)\\): (created|closed)")
 patternLS = re.compile("- LS\((\\d+)\\): (created|closed)")
 
+
 def statistical():
+    repeatCount = 0
     fo = open(logFile)
 
     setRS = set()
@@ -25,6 +27,7 @@ def statistical():
                 # print "created"
                 if m.group(1) in setRS:
                     print "error: %s has created" % (m.group(1))
+                    repeatCount += 1
                 else:
                     setRS.add(m.group(1))
             elif m.group(2) == "closed":
@@ -50,10 +53,30 @@ def statistical():
                         print "error: %s has closed" % (m.group(1))
                     else:
                         setLS.remove(m.group(1))
+    fo.close();
     print "Remote socket :"
     print setRS
     print "Local socket :"
     print setLS
+    print "RS: %d + LS: %d = %d" % (len(setRS), len(setLS), int(len(setRS) + len(setLS)))
+    print "repeatCount RS:%d" % (repeatCount)
+    isFirst = True
+
+    grepLSCmd = "grep -nE \""
+    for localSocket in setLS:
+        if isFirst:
+            isFirst = False
+            grepLSCmd += "LS\(%s\)" % (localSocket)
+        else:
+            grepLSCmd += "|LS\(%s\)" % (localSocket)
+    grepLSCmd += "\" " + logFile
+    # print grepLSCmd
+    grepScriptFilePath = "/tmp/grepLSCmd.sh"
+    grepScript = open(grepScriptFilePath, "w")
+    grepScript.write(grepLSCmd)
+    grepScript.close()
+    print "cp %s ." % (grepScriptFilePath)
+
 
 def usage():
     print 'usage:\n' \
@@ -64,7 +87,7 @@ def usage():
 
 def handleOpt():
     try:
-        options, args = getopt.getopt(sys.argv[1:], "f:i:p:", ['file=', "ip=", "port="])
+        options, args = getopt.getopt(sys.argv[1:], "f:g:p:", ['file=', "grep=", "port="])
         # print args
         if len(options) == 0:
             usage()
@@ -74,8 +97,9 @@ def handleOpt():
             if name in ('-f', '--file'):
                 global logFile
                 logFile = value
-            elif name in ('-i', '--ip'):
+            elif name in ('-g', '--grep'):
                 print value
+
             elif name in ('-p', '--port'):
                 print value
     except getopt.GetoptError:
