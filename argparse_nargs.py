8d171d9de7b9a458fed2988ae7a9dedc41f575ca
Leocone <wangliang@testbird.com>
2018-06-12 09:59:45 +0800

    argparse_nargs.py add __main__

--- argparse_nargs.py
+++ argparse_nargs.py
@@ -1,26 +1,26 @@
 import argparse
 
 # http://blog.xiayf.cn/2013/03/30/argparse/
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
 
-parser = argparse.ArgumentParser()
+    parser.add_argument('--three', nargs=3)
+    parser.add_argument('--optional', nargs='?')
+    parser.add_argument('--all', nargs='*', dest='all')
+    parser.add_argument('--one-or-more', nargs='+')
 
-parser.add_argument('--three', nargs=3)
-parser.add_argument('--optional', nargs='?')
-parser.add_argument('--all', nargs='*', dest='all')
-parser.add_argument('--one-or-more', nargs='+')
+    result = parser.parse_args()
 
-result = parser.parse_args()
+    print result
 
-print result
+    if result.three:
+        print result.three
 
-if result.three:	
-	print result.three
+    if result.optional:
+        print result.optional
 
-if result.optional:
-	print result.optional
+    if result.all:
+        print result.all
 
-if result.all:
-	print result.all
-
-if result.one_or_more:
-	print result.one_or_more
+    if result.one_or_more:
+        print result.one_or_more
