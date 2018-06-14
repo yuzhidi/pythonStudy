4f0c40d7785f5b047af04c73a3d367b8ef1b24b7
Leocone <wangliang@testbird.com>
2018-06-14 10:21:38 +0800

    os.path.dirname, os.path.basename

--- OsTest.py
+++ OsTest.py
@@ -1,2 +1,10 @@
 import os
 print os.getcwd()
+
+print("get pathname:",os.path.dirname('/Users/liuxiaolong/PycharmProjects/untitled/sw724.vaps'))
+print("get filename:",os.path.basename('/Users/liuxiaolong/PycharmProjects/untitled/sw724.vaps'))
+print("get pathname:",os.path.dirname('sw724.vaps'))
+print("get filename:",os.path.basename('sw724.vaps'))
+
+print("get pathname:",os.path.dirname('/sw724.vaps/'))
+print("get filename:",os.path.basename('/sw724.vaps/'))
