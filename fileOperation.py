4777166f201eb81a0e05e3697a3ac8651ddd514b
Leocone <wangliang@testbird.com>
2018-07-24 17:34:01 +0800

    more fileOperation.py urllib2Test.py

--- fileOperation.py
+++ fileOperation.py
@@ -2,10 +2,57 @@ fileHandle = open ( '/tmp/pythonFileOpTest.txt', 'w' )
 fileHandle.write ( 'This is a test.\nReally, it is.\r\n' ) 
 fileHandle.close() 
 
-with open('/tmp/pythonFileOpTest.txt') as fp:
-    for line in iter(fp.readline, ''):
-    	print line
+# with open('/Users/wangliang/Study/HotPot/tbRemote.py') as fp:
+#     for line in iter(fp.readline, ''):
+#     	print line
 
-    	with open('/tmp/pythonFileOpTest.txt') as fp:
-    		for line in iter(fp.readline, ''):
-    			print line
+
+print '>>>>>>>>>>>>>>>>>>>> no iter'
+fileHandle = open ( '/tmp/pythonFileOpTest.txt', 'w' )
+toMatch = 0
+braceMatched = 0
+leftBrace = 0
+rightBrace = 0
+
+def checkBrace(line):
+	global toMatch
+	global braceMatched
+	global leftBrace
+	global rightBrace
+	leftBrace = leftBrace + line.count('{')
+	rightBrace = rightBrace + line.count('}')
+	print 'leftBrace:' + str(leftBrace)
+	print 'rightBrace:' + str(rightBrace)
+	if leftBrace and leftBrace == rightBrace:
+		print '+++++++++++++++++++++++++++1 :' + str(leftBrace)
+		braceMatched = braceMatched + 1
+
+	if rightBrace and toMatch == braceMatched:
+		print '****************************reset :'
+		print toMatch
+		print braceMatched
+		toMatch = 0
+		braceMatched = 0
+		leftBrace = 0
+		rightBrace = 0
+
+	print 'checkEnd ' + str(toMatch)
+
+with open('/Users/wangliang/Study/HotPot/3318bf0a912a460cb1f372a5d21ac126/roc/roc.log.20180723090320.aaag-keyword-3318bf0a912a460cb1f372a5d21ac126-udid-e3c78110ca75220f0a7ce2c8a9b66a2a86564759') as fp:
+	for line in fp.readlines():
+		if toMatch:
+			print line
+			fileHandle.write(line)
+			checkBrace(line)
+
+
+		if 'WDACommunication' in line:
+			print line
+			fileHandle.write (line) 
+			if 'responseJson' in line  or 'doPost' in line or 'getAlertInfo' in line:
+				print '>>>>>>>>>>>>>> toMatch=1'
+				toMatch = 1
+				checkBrace(line)
+
+
+fileHandle.close()
--- urllib2Test.py
+++ urllib2Test.py
@@ -28,13 +28,51 @@ testfile.retrieve("http://quail.lab.tb/api/task/execution/mYMkKX/", "/tmp/mYYYYY
 
 print ' >>>>>>>>>>>>>>>>>> import requests'
 import requests
-url = "http://file.lab.tb/upload/aea0d0688dc246ab8809c1dfac45e2af.js"
+
+print '>>>>>>>>>>>>>>>>>>>>>>>>> test HTTP 404 Not Found'
+url = 'http://quail.lab.tb/api/testcase/app/2832cb97e74f44369efd2d460d4/'
 r = requests.get(url)
+print r.ok
+print r.content
+
+# urllib2.tocke
+# r = urllib2.urlopen(url)
+# print(r.read())
+
+print '################## get token'
+quail_user_email = 'xxx'
+quail_user_password ='tb123456'
+ObtainAuthTokenAPI = 'http://quail.lab.tb/api/customer/obtain-auth-token/?email=%s&password=%s' % (quail_user_email, quail_user_password)
+r = requests.get(ObtainAuthTokenAPI)
+print r.ok
+
+quail_user_email = 'xubo@testbird.com'
+ObtainAuthTokenAPI = 'http://quail.lab.tb/api/customer/obtain-auth-token/?email=%s&password=%s' % (quail_user_email, quail_user_password)
+r = requests.get(ObtainAuthTokenAPI)
+print r.ok
+
+TOKEN = 'dc8847f0b5b7334b39cbfe13094ad4e61ed8500b'
+HEADERS = {'Authorization': 'token {}'.format(TOKEN)}
+
+print '############################### test not exist'
+url = 'http://quail.lab.tb/api/testcase/app/2832cb369efd2d460d40ae9a/'
+r = requests.get(url, headers=HEADERS)
+print r.ok
+print r.content
+
+print '############################### test ok'
+url = 'http://quail.lab.tb/api/testcase/app/2832cb97e74f44369efd2d460d40ae9a/'
+r = requests.get(url, headers=HEADERS)
+print r.ok
 print len(r.content)
 
-url = 'http://quail.lab.tb/api/task/execution/mYMkKX'
-r = requests.get(url)
-print len(r.content)
+# url = "http://file.lab.tb/upload/aea0d0688dc246ab8809c1dfac45e2af.js"
+# r = requests.get(url)
+# print len(r.content)
+
+# url = 'http://quail.lab.tb/api/task/execution/mYMkKX'
+# r = requests.get(url)
+# print len(r.content)
 
 
 
