d83f1ab91367355b0e3702cea77c177c3fab35b2
Leocone <wangliang@testbird.com>
2018-12-07 18:02:41 +0800

    dictTest.py

--- dictTest.py
+++ dictTest.py
@@ -1,5 +1,13 @@
 
 device={}
 device['name'] = 'zte'
+device['haha'] = 'haha value'
+device['kaka'] = 'kaka value'
 
-print device
+print device.keys(), device.values()
+
+for i in device.keys():
+	print i
+
+for i in device.values():
+	print i
